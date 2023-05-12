40503c9b9d4f67b04d205d0f4eccc1f4f34d157b
aprameyaneopane <aprameyaneopane@gmail.com>
2023-05-12 15:42:37 +0545

    commends addition in progress

--- app/src/main/java/com/example/aprameya_todo_app/Database/TodoDatabase.kt
+++ app/src/main/java/com/example/aprameya_todo_app/Database/TodoDatabase.kt
@@ -7,11 +7,23 @@ import androidx.room.RoomDatabase
 import com.example.aprameya_todo_app.Models.Todo
 import com.example.aprameya_todo_app.utilities.DATABASE_NAME
 
+/**
+ * Database class to get database
+ *
+ * ROOM database is used
+ *
+ */
 @Database(entities = arrayOf(Todo::class), version = 1, exportSchema = false)
 abstract class TodoDatabase : RoomDatabase() { //implements class here
 
     //Singleton pattern used here
 
+    /**
+     * abstract class with no body
+     * but will be used to get database
+     *
+     * @return
+     */
     abstract fun getTodoDB() : TodoDB
 
     companion object{
@@ -19,6 +31,12 @@ abstract class TodoDatabase : RoomDatabase() { //implements class here
         @Volatile
         private var INSTANCE : TodoDatabase? = null
 
+        /**
+         * builds the database and returns an instance
+         *
+         * @param context
+         * @return
+         */
         fun getDatabase(context : Context) : TodoDatabase{
 
             return INSTANCE ?: synchronized(this){
